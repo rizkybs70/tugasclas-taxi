8f7532d3c10252f27405ddf1ce35b7bee2aee820
Rizky Bintang S <malingkoper@yahoo.com>
2021-03-16 22:19:09 +0700

    Upload Final

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Tugas_Class_Taxi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            taxi data1 = new taxi();
+
+            data1.DriverName = "Jono";
+            data1.OnDuty = true;
+            data1.NumPassenger = 10;
+
+            data1.TaxiInfo();
+            data1.PickUpPassenger();
+            data1.DropOffPassenger();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ taxi.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tugas_Class_Taxi
+{
+    class taxi
+    {
+        public string DriverName { get; set; }
+        public bool OnDuty { get; set; }
+        public int NumPassenger { get; set; }
+
+        public void TaxiInfo()
+        {
+            Console.WriteLine("Driver Name: {0}", DriverName);
+            Console.WriteLine("On Duty: {0}", OnDuty);
+            Console.WriteLine("Number of Passenger: {0}\n", NumPassenger);
+        }
+
+        public void PickUpPassenger()
+        {
+            Console.WriteLine("{0} sedang menjemput penumpang", DriverName);
+        }
+
+        public void DropOffPassenger()
+        {
+            Console.WriteLine("{0} selesai mengatar penumpang", DriverName);
+        }
+    }
+}
